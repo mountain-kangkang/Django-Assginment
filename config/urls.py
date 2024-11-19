3dedd674cd76554aca45d1179e3036d4d81c27e8
mountain-kangkang <rngl10@naver.com>
2024-11-19 17:17:39 +0900

    아.. 뭔가 잘 못 수정함...

--- config/urls.py
+++ config/urls.py
@@ -15,21 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.http import Http404
 from django.shortcuts import render
-from django.urls import path
 # from config.fake_db import user_db
 from todo.views import todo_list, todo_info
-#
+from django.urls import path, include
+from django.contrib import admin
 #
 # _db = user_db
-#
 #
 # def user_list(request):
 #     names = [{'id': key, 'name': value['이름']} for key, value in _db.items()]
 #     return render(request, 'user_list.html', {'data': names})
-#
 #
 # def user_info(request, user_id):
 #     if user_id > len(_db):
@@ -37,12 +34,12 @@ from todo.views import todo_list, todo_info
 #     info = _db[user_id]
 #     return render(request, 'user_info.html', {'data': info})
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('todo/', todo_list, name='todo_list'),
-    path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path("admin/", admin.site.urls),
     # path('users/', user_list, name='user_list'),
     # path('users/<int:user_id>/', user_info, name='user_info'),
+    path("todo/", todo_list, name='todo_list'),
+    path("todo/<int:todo_id>/", todo_info, name='todo_info'),
+    path("accounts/", include('django.contrib.auth.urls')),
 ]
 
