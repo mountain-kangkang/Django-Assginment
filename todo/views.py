f248d1c82f289d82ebd313f23f2b67defd1e3533
mountain-kangkang <rngl10@naver.com>
2024-12-10 15:58:49 +0900

    todo list 검색기능

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 from todo.models import Todo
 
-# @login_required()
+@login_required()
 def todo_list(request):
     todos = Todo.objects.filter(author=request.user)
+
+    q = request.GET.get('q')
+    if q:
+        todos = todos.filter(
+            Q(title__icontains=q) |
+            Q(description__icontains=q)
+        )
+
     context = {'todos':todos}
     return render(request, 'todo_list.html', context)
 
